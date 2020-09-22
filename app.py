590941e29114c48bfad060f7b36dc8a2caedcc6f
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-09-22 12:18:03 -0700

    testing

--- app.py
+++ app.py
@@ -185,3 +185,8 @@ def indexDatabase(index):
     if index >= len(database) or index < 1:
         return {'result':"No entry at index "+ str(index)}
     return {'result': database[index]}
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", threaded=True, port=5000)
